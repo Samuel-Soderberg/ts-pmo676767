30871812e6a8013088596558b9b10a3cb2955497
Samuel <samuel.soderberg2@elev.ga.lbs.se>
2026-01-09 13:58:45 +0100

    testar ljus och gjorde movement scriptet fungerande

--- ts pmo676767/Assets/Player.cs
+++ ts pmo676767/Assets/Player.cs
@@ -3,38 +3,32 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
+    [SerializeField]
     int speed = 1;
+    Vector2 dir;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
-
     void Update()
     {
+        dir = Vector2.zero;
         if (Input.GetKey(KeyCode.UpArrow))
         {
-
-
+            dir.y += 1;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-
-
+            dir.y -= 1;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-
-
+            dir.x -= 1;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-
-
+            dir.x += 1;
         }
-        else
-        {
-            speed = 0;
-        }
+        rb.linearVelocity = dir * speed;
     }
 }
