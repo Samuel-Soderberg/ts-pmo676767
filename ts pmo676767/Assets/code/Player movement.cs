cce8478deff3ce10d3f1d58812cbf5c8e26d6485
Samuel <samuel.soderberg2@elev.ga.lbs.se>
2026-01-09 20:35:27 +0100

    acceleration och deceleration i movement scriptet

--- ts pmo676767/Assets/code/Player movement.cs
+++ ts pmo676767/Assets/code/Player movement.cs
@@ -2,18 +2,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Vector2 currentVelocity;
     Rigidbody2D rb;
-    Vector2 dir;
+    Vector2 direction;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
     void FixedUpdate()
     {
-        rb.linearVelocity = dir * PlayerSettings.Instance.speed;
+        Vector2 targetVelocity = direction * PlayerSettings.Instance.maxspeed;
+        if (direction.magnitude > 0)
+        {
+            currentVelocity = Vector2.MoveTowards(currentVelocity, targetVelocity, PlayerSettings.Instance.acceleration * Time.fixedDeltaTime);
+        }
+        else
+        {
+            currentVelocity = Vector2.MoveTowards(currentVelocity, Vector2.zero, PlayerSettings.Instance.deceleration * Time.fixedDeltaTime);
+        }
+        rb.linearVelocity = currentVelocity;
     }
 }
--- ts pmo676767/Assets/code/setting classes/player settings.cs
+++ ts pmo676767/Assets/code/setting classes/player settings.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class PlayerSettings : MonoBehaviour 
 {
     [Header("Player Movement")]
-    public float speed = 5f;
+    public float maxspeed = 5f;
+    public float acceleration = 10f;
+    public float deceleration = 10f;
 
-    
     public static PlayerSettings Instance;
     void Awake()
     {
