ebe38e5b83d00b9922922fa705062a495ecb7409
MrELVINO <elvin.masus@elev.ga.lbs.se>
2026-01-09 15:47:12 +0100

    START

---
+++ ts pmo676767/Assets/code/start.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour
+{
+    public string scenn;
+    public void scennchanger()
+    {
+        SceneManager.LoadScene(scenn);
+    }
+
+
+
+
+
+
+}
